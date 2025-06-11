d4e1ef924b02814145a20af625fc1bc69bc74e2b
Zeus999-afk <mesyyyou@gmail.com>
2025-06-12 00:37:55 +0700

    Second commit

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { myColors} from './src/style/Colors'; 
+import { ThemeContext } from './src/context/ThemeContext';
+import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
   return (
